88c802d8f74268ed8892f867c9b1e5f04700e01d
codect0 <team@codecto.ai>
2026-02-12 01:39:09 +0100

    Update passport.js

--- core/passport.js
+++ core/passport.js
@@ -2,7 +2,7 @@ var db = require('../models')
 var LocalStrategy = require('passport-local').Strategy
 var bCrypt = require('bcrypt')
 
-
+#test
 module.exports = function (passport) {
 
     passport.serializeUser(function (user, done) {
